ab1661f1e0417c5b3cb41def9c00aedc245d0eac
samuel <samuelhermany@hotmail.com>
2024-10-28 00:40:13 -0300

    exe 09

--- screens/TP1/09/index.jsx
+++ screens/TP1/09/index.jsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+
 
 export default function App() {
+    const [value, setValue] = useState('');
+    const [tipo, setTipo] = useState('');
+
+    const verificar = () => {
+        const numero = parseInt(value); // Converte o valor para um número
+        if (numero %2 == 0) {
+            setTipo(`O número ${value} é par`);
+            setValue('');
+        } else {
+            setTipo(`O número ${value} é ímpar`);
+            setValue('');
+        }
+    }
+
     return (
         <View>
-            <Text>Hello World</Text>
+            <TextInput
+                placeholder='Digite um número'
+                keyboardType='numeric'
+                value={value}
+                onChangeText={setValue}/>
+            <Button title='Verificar' onPress={verificar}/>
+            <Text>{tipo}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    imagem: {
+      width: 150,
+      height: 150,
+    },
+    titulo: {
+      marginBottom: 20,
+    },
+  });
