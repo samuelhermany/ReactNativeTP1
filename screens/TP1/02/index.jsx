40df70d53d8b4727c5ae1aeef8cf9c28a49ea4cb
samuel <samuelhermany@hotmail.com>
2024-10-27 22:23:24 -0300

    exe01 e 02

--- screens/TP1/02/index.jsx
+++ screens/TP1/02/index.jsx
@@ -1,9 +1,21 @@
 import { View, Text } from 'react-native';
 
 export default function App() {
+    const seuNomeCompleto = "Samuel Hermany";
+    const horaAtual = () => {
+        const time = new Date();
+        return time.toLocaleTimeString('pt-BR', {
+            hour: '2-digit',
+            minute: '2-digit',
+            hourCycle: 'h23',
+            timeZone: 'America/Sao_Paulo'
+        });
+    };
+
+    const texto = `Olá, ${seuNomeCompleto}! Agora são ${horaAtual()}`
     return (
         <View>
-            <Text>Hello World</Text>
+            <Text>{texto}</Text>
         </View>
     )
 }
