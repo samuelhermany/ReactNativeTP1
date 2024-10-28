a60764a633a54e85bb5eeb635d557abb32666b84
samuel <samuelhermany@hotmail.com>
2024-10-27 23:10:24 -0300

    exe 05

--- screens/TP1/03/index.jsx
+++ screens/TP1/03/index.jsx
@@ -1,9 +1,35 @@
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet  } from 'react-native';
 
 export default function App() {
+  const [corBotao, setCorBotao] = useState('');
+
+  const handlePressButton = (cor) => {
+    setCorBotao(cor);
+  };
+
     return (
         <View>
-            <Text>Hello World</Text>
+            <View >
+                <Button style={styles.btnVermelho} title="Vermelho" onPress={() => handlePressButton('Vermelho')} />
+                <Button title="Verde" onPress={() => handlePressButton('Verde')} />
+                <Button title="Azul" onPress={() => handlePressButton('Azul')} />
+            </View>
+            <Text >
+                Botão {corBotao} pressionado!
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    btnVermelho: {
+        backgroundColor: 'red',
+      },
+    btnVerde: {
+        backgroundColor: 'red',
+      },
+    btnAzul: {
+      backgroundColor: 'red',
+    },
+  });
--- screens/TP1/05/index.jsx
+++ screens/TP1/05/index.jsx
@@ -1,9 +1,57 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function App() {
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(prevCounter => prevCounter + 1)
+    };
+
+    const decrement = () => {
+        setCounter(prevCounter => prevCounter - 1)
+    };
+
     return (
         <View>
-            <Text>Hello World</Text>
+            <View style={styles.botoes}>
+                <TouchableOpacity style={styles.button} onPress={increment}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={decrement}>
+                    <Text style={styles.buttonText}>-</Text>
+                </TouchableOpacity>
+            </View>
+                <Text style={styles.counterText}>{counter}</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    botoes: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+        gap: 10,
+    },
+    button: {
+        backgroundColor: '#007BFF',
+        padding: 10,
+        borderRadius: 5,
+        width: 30,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+    counterText: {
+        fontSize: 30,
+        margin: 'auto',
+    },
+})
