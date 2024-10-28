8902e4bd01e75e9681e39ecdeca6afad4b855af3
samuel <samuelhermany@hotmail.com>
2024-10-28 01:01:53 -0300

    exe 11

--- screens/TP1/11/index.jsx
+++ screens/TP1/11/index.jsx
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function App() {
-    return (
-        <View>
-            <Text>Hello World</Text>
-        </View>
-    )
+  const [value, setValue] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  const verificarPalindromo = () => {
+    const textoLimpo = value.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    // Inverte o texto
+    const textoReverso = textoLimpo.split('').reverse().join('');
+
+    if (textoLimpo === textoReverso) {
+      setResultado(`"${value}" é um palíndromo!`);
+    } else {
+      setResultado(`"${value}" não é um palíndromo.`);
+    }
+
+    setValue('');
+  };
+
+  return (
+    <View >
+      <TextInput
+        placeholder='Digite uma palavra ou frase'
+        value={value}
+        onChangeText={setValue}
+      />
+      <Button title='Verificar' onPress={verificarPalindromo} />
+      <Text >{resultado}</Text>
+    </View>
+  );
 }
